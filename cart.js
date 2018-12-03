0264be0f2e0f2dd5c21ac723599ac999d647c698
Yin Xiaoyu <xiaoyu@teambition.com>
2018-12-03 16:09:14 +0800

    feat: Impl lab5 - getCart

--- cart.js
+++ cart.js
@@ -38,19 +38,16 @@ function CartDAO(database) {
         *
         */
 
-        var userCart = {
-            userId: userId,
-            items: []
-        }
-        var dummyItem = this.createDummyItem();
-        userCart.items.push(dummyItem);
-
-        // TODO-lab5 Replace all code above (in this method).
-
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the userCart to the
-        // callback.
-        callback(userCart);
+        this.db.collection('cart').findOne({"userId": userId},
+            (err, result) => {
+                if (err) {
+                    console.log(err)            
+                    return
+                }
+                console.log(result)            
+                callback(result)
+            }
+        )          
     }
 
 
