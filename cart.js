39adce7edcd524b4e65f300b8d2a523053c49003
Yin Xiaoyu <xiaoyu@teambition.com>
2018-12-03 18:17:48 +0800

    feat: Impl lab7 updateQuantity

--- cart.js
+++ cart.js
@@ -25,7 +25,7 @@ function CartDAO(database) {
     this.db = database;
 
 
-    this.getCart = function(userId, callback) {
+    this.getCart = function (userId, callback) {
         "use strict";
 
         /*
@@ -38,20 +38,20 @@ function CartDAO(database) {
         *
         */
 
-        this.db.collection('cart').findOne({"userId": userId},
+        this.db.collection('cart').findOne({ "userId": userId },
             (err, result) => {
                 if (err) {
-                    console.log(err)            
+                    console.log(err)
                     return
                 }
-                console.log(result)            
+                console.log(result)
                 callback(result)
             }
-        )          
+        )
     }
 
 
-    this.itemInCart = function(userId, itemId, callback) {
+    this.itemInCart = function (userId, itemId, callback) {
         "use strict";
 
         /*
@@ -80,8 +80,8 @@ function CartDAO(database) {
          */
         let collection = this.db.collection('cart')
         collection.findOne(
-            {"userId": userId, "items._id": itemId},
-            {"projection": {"items.$": 1}},
+            { "userId": userId, "items._id": itemId },
+            { "projection": { "items.$": 1 } },
             (err, result) => {
                 if (err) {
                     console.log(err)
@@ -96,7 +96,7 @@ function CartDAO(database) {
                     callback(result.items[0])
                 }
             }
-        )                
+        )
     }
 
 
@@ -118,15 +118,15 @@ function CartDAO(database) {
      * http://mongodb.github.io/node-mongodb-native/2.0/api/Collection.html#findOneAndUpdate
      *
      */
-    this.addItem = function(userId, item, callback) {
+    this.addItem = function (userId, item, callback) {
         "use strict";
 
         // Will update the first document found matching the query document.
         this.db.collection("cart").findOneAndUpdate(
             // query for the cart with the userId passed as a parameter.
-            {userId: userId},
+            { userId: userId },
             // update the user's cart by pushing an item onto the items array
-            {"$push": {items: item}},
+            { "$push": { items: item } },
             // findOneAndUpdate() takes an options document as a parameter.
             // Here we are specifying that the database should insert a cart
             // if one doesn't already exist (i.e. "upsert: true") and that
@@ -139,7 +139,7 @@ function CartDAO(database) {
             },
             // Because we specified "returnOriginal: false", this callback
             // will be passed the updated document as the value of result.
-            function(err, result) {
+            function (err, result) {
                 assert.equal(null, err);
                 // To get the actual document updated we need to access the
                 // value field of the result.
@@ -166,7 +166,7 @@ function CartDAO(database) {
     };
 
 
-    this.updateQuantity = function(userId, itemId, quantity, callback) {
+    this.updateQuantity = function (userId, itemId, quantity, callback) {
         "use strict";
 
         /*
@@ -185,21 +185,25 @@ function CartDAO(database) {
         * https://docs.mongodb.org/manual/reference/operator/update/positional/
         *
         */
-
-        var userCart = {
-            userId: userId,
-            items: []
+        let collection = this.db.collection('cart')
+        let query = {"userId": userId, "items._id": itemId}
+        let update = quantity === 0 ? 
+            { "$pull": { "items": { "_id": itemId } } } :
+            { "$set": {"items.$.quantity": quantity } }
+        let options = { returnOriginal: false }
+        let processFn = (err, result) => {
+            assert.equal(null, err)
+            console.log(result.value)
+            callback(result.value)
         }
-        var dummyItem = this.createDummyItem();
-        dummyItem.quantity = quantity;
-        userCart.items.push(dummyItem);
-        callback(userCart);
-
-        // TODO-lab7 Replace all code above (in this method).
-
+        
+        collection.findOneAndUpdate(query,
+            update,
+            options,
+            processFn)
     }
 
-    this.createDummyItem = function() {
+    this.createDummyItem = function () {
         "use strict";
 
         var item = {
