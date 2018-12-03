08d36af492b738dd1df2f1eb2a0dcc76bb2226b1
Yin Xiaoyu <xiaoyu@teambition.com>
2018-12-03 16:07:13 +0800

    feat: impl lab4 - addReview

--- items.js
+++ items.js
@@ -267,38 +267,42 @@ function ItemDAO(database) {
             });
     };
 
-
+    /*
+    * TODO-lab4
+    *
+    * LAB #4: Implement addReview().
+    *
+    * Using the itemId parameter, update the appropriate document in the
+    * "item" collection with a new review. Reviews are stored as an
+    * array value for the key "reviews". Each review has the fields:
+    * "name", "comment", "stars", and "date".
+    *
+    */
     this.addReview = function (itemId, comment, name, stars, callback) {
         "use strict";
 
-        /*
-         * TODO-lab4
-         *
-         * LAB #4: Implement addReview().
-         *
-         * Using the itemId parameter, update the appropriate document in the
-         * "item" collection with a new review. Reviews are stored as an
-         * array value for the key "reviews". Each review has the fields:
-         * "name", "comment", "stars", and "date".
-         *
-         */
-
-        var reviewDoc = {
+        let reviewDoc = {
             name: name,
             comment: comment,
             stars: stars,
             date: Date.now()
         }
 
-        // TODO replace the following two lines with your code that will
-        // update the document with a new review.
-        var doc = this.createDummyItem();
-        doc.reviews = [reviewDoc];
+        this.db.collection('item').updateOne({"_id": itemId}, 
+            {"$push": 
+                {"reviews": reviewDoc}
+            }, 
+            (err, result) => {
+                if (err) {
+                    console.log(err)
+                    callback(null)
+                    return
+                } 
 
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the updated doc to the
-        // callback.
-        callback(doc);
+                // console.log(result)
+                callback(result)
+            }                
+        )
     }
 
 
